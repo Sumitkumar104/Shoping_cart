eac50ff9734faf7eb9bdd66c85968ed6feca6415
Sumitkumar104 <parjapatsumit104@gmail.com>
2023-12-21 22:05:57 +0530

    Create Home Item Using Fetchdata From API

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from 'react';
 
-export default function Product() {
+export default function Product({ data }) {
+  const styles = {
+    container: {
+      border: '1px solid #ccc',
+      borderRadius: '8px',
+      padding: '16px',
+      margin: '16px',
+      width: '300px',
+    },
+    title: {
+      fontSize: '1.2rem',
+      fontWeight: 'bold',
+    },
+    description: {
+      margin: '8px 0',
+    },
+    image: {
+      maxWidth: '100%',
+      height: 'auto',
+      marginBottom: '8px',
+    },
+    price: {
+      color: 'green',
+      fontWeight: 'bold',
+    },
+  };
+
   return (
-    <div>
-      
+    <div style={styles.container}>
+      <div style={styles.title}>{data.title}</div>
+      <p style={styles.description}>{data.description}</p>
+      <img style={styles.image} src={data.image} alt="Product" />
+      <p style={styles.price}>{`$${data.price}`}</p>
     </div>
-  )
+  );
 }
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import Product from '../Components/Product';
 
 const Home = () => {
-  return (
-    <div>
-      i am in home
-    </div>
-  )
-}
+  const Api_Url = 'https://fakestoreapi.com/products';
+  const [dataa, setdata] = useState([]);
 
-export default Home
+  async function fetchdata() {
+    try {
+      const result = await fetch(Api_Url);
+      const data = await result.json();
+      setdata(data);
+      console.log('fetchdata');
+      console.log(data[1].title); // Access data directly, not dataa
+    } catch (error) {
+      console.log('There is an error fetching data:', error);
+      setdata([]);
+    }
+  }
+
+  useEffect(() => {
+    fetchdata();
+  }, []);
+
+  return (
+    <div className=''>
+      {dataa.map((data) => (
+        <Product key={data.id} data={data} />
+      ))}
+    </div>
+  );
+};
+
+export default Home;
